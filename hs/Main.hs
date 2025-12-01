672d7c87e90ca7c6d17dedf68d995acef5dca009
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-01 19:50:20 +0100

    feat: NeuralNetwork data class

--- hs/Main.hs
+++ hs/Main.hs
@@ -5,7 +5,7 @@
   Simple Neural Network for Handwriting Recognition
   Written in pure Haskell - no ML libraries!
 
-  Architecture: Input(784) → Hidden(128, ReLU) → Output(36, Softmax)
+  Architecture: Input(784) -> Hidden(128, ReLU) -> Output(36, Softmax)
   Recognizes: A-Z (0-25) and 0-9 (26-35)
 
   Training data: EMNIST dataset (prepared by prepare_data.py)
@@ -15,3 +15,16 @@ module Main where
 
 type Vector = [] Double
 type Matrix = [] [] Double
+
+data NeuralNetwork = NeuralNetwork
+  { weightsIH :: !Matrix    -- Input to Hidden (784 x 128)
+  , biasH     :: !Vector    -- Hidden bias (129)
+  , weightsHO :: !Matrix    -- Hidden to Output (128 * 36)
+  , biasO     :: !Vector    -- Output bias (36)
+  } deriving (Show, Read)
+
+data Prediction = Prediction
+  {
+  }
+
+
